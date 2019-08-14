ef441485cdcc5f108264934a7606e79a4cd22c1c
David Sanders <davesque@gmail.com>
2019-08-14 01:20:38 -0600

    Add beginnings of vyper parser

--- parser/src/vyper/ast.rs
+++ parser/src/vyper/ast.rs
@@ -1,27 +1,27 @@
 pub type Name = String;
 pub type Type = String;
 
+#[derive(Debug, PartialEq)]
 pub struct Module {
     pub body: Vec<ModuleStmt>,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum ModuleStmt {
-    EventDef {
-        name: Name,
-        fields: Vec<EventField>,
-    },
+    EventDef { name: Name, fields: Vec<EventField> },
     //InterfaceDef {
     //    name: Name,
     //    fields: Vec<InterfaceField>,
     //    methods: Vec<InterfaceMethodDef>,
     //},
-    ContractDef {
-        name: Name,
-        //fields: Vec<ContractField>,
-        methods: Vec<ContractMethodDef>,
-    },
+    //ContractDef {
+    //    name: Name,
+    //    fields: Vec<ContractField>,
+    //    methods: Vec<ContractMethodDef>,
+    //},
 }
 
+#[derive(Debug, PartialEq)]
 pub struct EventField {
     pub name: Name,
     pub typ: Type,
@@ -46,19 +46,19 @@ pub struct EventField {
 //    pub return_type: Option<Type>,
 //}
 
-pub struct ContractMethodDef {
-    pub name: Name,
-    //pub props: Vec<MethodProps>,
-    pub args: Vec<Arg>,
-    //pub return_type: Option<Type>,
-    pub body: Option<Stmt>,
-}
+//pub struct ContractMethodDef {
+//    pub name: Name,
+//    pub props: Vec<MethodProps>,
+//    pub args: Vec<Arg>,
+//    pub return_type: Option<Type>,
+//    pub body: Option<Stmt>,
+//}
 
-pub struct Arg {
-    pub name: Name,
-    pub typ: Type,
-    //pub default: Option<Expr>,
-}
+//pub struct Arg {
+//    pub name: Name,
+//    pub typ: Type,
+//    pub default: Option<Expr>,
+//}
 
 //pub enum MethodProps {
 //    Constant,
@@ -67,41 +67,41 @@ pub struct Arg {
 //    Private,
 //}
 
-pub enum Stmt {
-    //Return {
-    //    value: Option<Expr>,
-    //}
-    //Assign {
-    //    target: Vec<Expr>,
-    //    typ: Type,
-    //    value: Expr,
-    //},
-    //AugAssign {
-    //    target: Expr,
-    //    op: Operator,
-    //},
-    EmitEvent {
-        event_name: Name,
-        args: Vec<Expr>,
-    },
-}
+//pub enum Stmt {
+//    Return {
+//        value: Option<Expr>,
+//    }
+//    Assign {
+//        target: Vec<Expr>,
+//        typ: Type,
+//        value: Expr,
+//    },
+//    AugAssign {
+//        target: Expr,
+//        op: Operator,
+//    },
+//    EmitEvent {
+//        event_name: Name,
+//        args: Vec<Expr>,
+//    },
+//}
 
-pub enum Operator {
-    Add,
-    Sub,
-    Mult,
-    Div,
-    Mod,
-    Pow,
-    LShift,
-    RShift,
-    BitOr,
-    BitXor,
-    BitAnd,
-}
+//pub enum Operator {
+//    Add,
+//    Sub,
+//    Mult,
+//    Div,
+//    Mod,
+//    Pow,
+//    LShift,
+//    RShift,
+//    BitOr,
+//    BitXor,
+//    BitAnd,
+//}
 
-pub enum Expr {
-    Name {
-        id: Name,
-    },
-}
+//pub enum Expr {
+//    Name {
+//        id: Name,
+//    },
+//}
